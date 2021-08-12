bf49a4e57b113b042646a06797720de673045710
denrahydnas <sandylin34@gmail.com>
2021-08-12 17:27:07 -0400

    adding react - traversy video 2

--- leadmanager/leadmanager/settings.py
+++ leadmanager/leadmanager/settings.py
@@ -39,7 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'leads',
-    'rest_framework'
+    'rest_framework',
+    'frontend'
 ]
 
 MIDDLEWARE = [
@@ -121,6 +122,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
